2797c6802cb88151a720ca95209c0a6ac216e37f
Chandu5342 <karrichandu309@gmail.com>
2025-12-31 23:16:26 +0530

    afterauth

--- server.js
+++ server.js
@@ -2,14 +2,14 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
-
+import authRoutes from "./routes/auth.routes.js";
 dotenv.config();
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-
+app.use("/auth", authRoutes);
 // test route
 app.get("/", (req, res) => {
   res.status(200).json({
